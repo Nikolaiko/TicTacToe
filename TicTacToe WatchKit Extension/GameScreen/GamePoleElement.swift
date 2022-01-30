7333495d045db5519d670aa77d6d9c904c1ef601
Nikolai Baklanov <nikolaiko@mail.ru>
2022-01-30 07:55:14 +0600

    Game pole.

---
+++ TicTacToe WatchKit Extension/GameScreen/GamePoleElement.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct GamePoleElement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GamePoleElement_Previews: PreviewProvider {
+    static var previews: some View {
+        GamePoleElement()
+    }
+}
--- TicTacToe WatchKit Extension/GameScreen/GameScreen.swift
+++ TicTacToe WatchKit Extension/GameScreen/GameScreen.swift
@@ -1,9 +1,32 @@
 import SwiftUI
 
 struct GameScreen: View {
+    private let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 0),
+        GridItem(.flexible(), spacing: 0),
+        GridItem(.flexible(), spacing: 0)
+    ]
+    
     var body: some View {
         GeometryReader { geometry in
-            
+            VStack {
+                LazyVGrid(columns: columns) {
+                    ForEach(0..<9) { i in
+                        HStack {
+                            Rectangle()
+                                .foregroundColor(.red)
+                                .aspectRatio(1, contentMode: .fit)
+                                .overlay {
+                                    Image("cross")
+                                        .resizable()
+                                        .aspectRatio(1, contentMode: .fit)
+                                }
+                            Divider()
+                        }
+                    }
+                }
+            }
+            .background(.black)
         }
     }
 }
